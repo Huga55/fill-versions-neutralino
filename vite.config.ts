b45f322d6f4f4efabf51aee22501995798ded91f
Mihail <xp_92@mail.ru>
2025-02-26 01:52:13 +0300

    fix: add coping neutralino.js to dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
+import fs from "fs-extra";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 8080,
-  },
+export default defineConfig(async () => {
+  return {
+    server: {
+      port: 8080,
+    },
+    plugins: [
+      {
+        name: "copy-neutralino-js",
+        writeBundle() {
+          const sourcePath = path.resolve(__dirname, "neutralino.js");
+          const destPath = path.resolve(__dirname, "dist", "neutralino.js");
+
+          fs.copySync(sourcePath, destPath);
+
+          console.log("neutralino.js был скопирован в dist/");
+        },
+      },
+    ],
+  };
 });
